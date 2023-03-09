a4719fd8ac2a819f979812eb733f755ba431e8cb
Faizan <faizanalam980@gmail.com>
2023-03-09 19:42:36 +0530

    post component completed

--- src/Post.js
+++ src/Post.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import { Avatar } from '@mui/material'
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import NearMeIcon from '@mui/icons-material/NearMe';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import './Post.css'
 
 function Post({profilePic, image, username, timestamp, message}) {
@@ -19,6 +24,24 @@ function Post({profilePic, image, username, timestamp, message}) {
         </div>
         <div className="post_image">
             <img src={image} alt="" />
+        </div>
+        <div className="post_options">
+            <div className="post_option">
+                <ThumbUpIcon/>
+                <p>Like</p>
+            </div>
+            <div className="post_option">
+                <ChatBubbleOutlineIcon/>
+                <p>Comment</p>
+            </div>
+            <div className="post_option">
+                <NearMeIcon/>
+                <p>Share</p>
+            </div>
+            <div className="post_option">
+                <AccountCircleIcon/>
+                <ExpandMoreIcon/>
+            </div>
         </div>
     </div>
   )
