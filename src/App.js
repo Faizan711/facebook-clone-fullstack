cfd3ee44b12595a61166e10dd3159d914a6c3c56
Faizan <faizanalam980@gmail.com>
2023-03-10 19:43:11 +0530

    completed widget

--- src/App.js
+++ src/App.js
@@ -5,14 +5,20 @@ import Feed from './Feed';
 import Widget from './Widget';
 
 function App() {
+  const user = "Faizan";
+
   return (
     <div className="app">
+      {!user ? <h1>Login</h1> : 
+      <>
         <Header/>
         <div className="app_body">
             <Sidebar/>
             <Feed />
             <Widget />
         </div>
+      </>
+}
     </div>
   );
 }
